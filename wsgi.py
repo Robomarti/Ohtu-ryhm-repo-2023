f4786c0bd473069496a00ae1b7f71c541ebc51f5
Roberto Martinez Keisala <robomarti@outlook.com>
2023-11-20 11:36:02 +0200

    removed another database.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,2 @@
-#!/usr/bin/python3
-import sys
-import logging
-logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0,"/root/customer-account-automation/")
-
 from app import app as application
 app = application
