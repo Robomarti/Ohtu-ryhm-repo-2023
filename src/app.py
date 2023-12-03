8a1f5c0398274bd90cf6dc2cf33788fb68ed407c
CoolMcRad <santtu-12@hotmail.com>
2023-12-03 10:16:17 +0200

    Fixed testing environment booting as it should

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 """Creates the app and assigns it a secret_key variable from the .env file"""
 
-from os import getenv
+from os import getenv , environ
 from flask import Flask
 import sys
 
@@ -8,9 +8,11 @@ app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 if not getenv("SECRET_KEY"):
     print('NO SECRET KEY SET :<<', file=sys.stderr)
-    import src.tests.robot.testing_routes
 else:
     print('All is working :>', file=sys.stderr)
+if environ.get("FLASK_ENV") == "test":
+    print("Running in testing environment")
+    import src.tests.robot.testing_routes
 
 # we have to disable these pylint warnings since this order is
 # the way flask wants us to import things
--- src/tests/robot/testing_routes.py
+++ src/tests/robot/testing_routes.py
@@ -1,3 +1,4 @@
+from src.app import app
 import src.tests.in_memory_db as test_db
 
 @app.route("/db_initialize")
